b325722fa0df42599e052c144d145de9621f605c
angelbl11 <angelbl11@gmail.com>
2025-02-14 12:11:56 -0600

    deleted secrets

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../secrets/firebase.json");
-
-const firebaseConfig = process.env.FIREBASE_CONFIG
-  ? JSON.parse(process.env.FIREBASE_CONFIG)
-  : serviceAccount;
-
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
 });
 
 const db = admin.firestore();
