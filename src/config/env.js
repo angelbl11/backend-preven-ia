6caedaec1edc7ca84fa0fff76f03b8391932d28d
angelbl11 <angelbl11@gmail.com>
2025-02-14 12:06:45 -0600

    env added

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,5 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   UPLOAD_DIR: "uploads",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  FIREBASE_CONFIG: process.env.FIREBASE_CONFIG,
 };
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,12 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("../../secrets/firebase.json");
 
+const firebaseConfig = process.env.FIREBASE_CONFIG
+  ? JSON.parse(process.env.FIREBASE_CONFIG)
+  : serviceAccount;
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 const db = admin.firestore();
